d7d6799c5691e00b63908d9f158ee48b4a8a74c4
김동환 <1033513@naver.com>
2021-04-29 14:07:20 +0900

    메인 뷰 컨틀롤러 상측 작업 중

--- COOKID/COOKID/ViewControllers/EatingDataViewController.swift
+++ COOKID/COOKID/ViewControllers/EatingDataViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class EatingDataViewController: UIViewController {
 
+    
+    @IBOutlet var dayButton: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,14 +23,10 @@ class EatingDataViewController: UIViewController {
         // Use data from the view controller which initiated the unwind segue
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        
     }
-    */
-
+    
 }
