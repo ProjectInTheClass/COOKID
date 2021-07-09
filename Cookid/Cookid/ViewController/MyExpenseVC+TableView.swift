f4a53aac033b18856a642974c715b3cd73733b65
nyokki1119 <ghdtjrgus333@naver.com>
2021-07-09 23:38:19 +0900

    myExpenseView update

---
+++ Cookid/Cookid/ViewController/MyExpenseVC+TableView.swift
@@ -0,0 +1,92 @@
+//
+//  MyExpenseVC+TableView.swift
+//  Cookid
+//
+//  Created by Sh Hong on 2021/07/09.
+//
+
+import UIKit
+
+
+extension MyExpenseViewController :  UITableViewDataSource, UITableViewDelegate {
+    
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+                
+        if section == 0 {
+            return selectedDineInMeals.count
+        } else if section == 1 {
+            return selectedDineOutMeals.count
+        } else {
+            return selectedShopping.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath) as? CustomTableViewCell else { return UITableViewCell() }
+        
+        if !selectedDineInMeals.isEmpty {
+            if indexPath.section == 0 {
+                guard selectedDineInMeals[indexPath.row]?.price != nil
+                else {
+                    cell.label.text = "0"
+                    return cell
+                }
+                cell.label.text = String(selectedDineInMeals[indexPath.row]!.price) + "원"
+                cell.dateLabel.text = dateFormatter.string(from: selectedDineInMeals[indexPath.row]!.date)
+            }
+        }
+
+        if !selectedDineOutMeals.isEmpty {
+            if indexPath.section == 1 {
+                guard selectedDineOutMeals[indexPath.row]?.price != nil
+                else {
+                    cell.label.text = "0"
+                    return cell
+                }
+                cell.label.text = String(selectedDineOutMeals[indexPath.row]!.price) + "원"
+                cell.dateLabel.text = dateFormatter.string(from: selectedDineOutMeals[indexPath.row]!.date)
+
+            }
+        }
+        
+        if !selectedShopping.isEmpty {
+            if indexPath.section == 2 {
+                guard selectedShopping[indexPath.row]?.totalPrice(groceries: selectedShopping[indexPath.row]!.groceries) != nil
+                else {
+                    cell.label.text = "0"
+                    return cell
+                }
+                cell.label.text = String(selectedShopping[indexPath.row]!.totalPrice(groceries: selectedShopping[indexPath.row]!.groceries)) + "원"
+                cell.dateLabel.text = dateFormatter.string(from: selectedShopping[indexPath.row]!.date)
+
+            }
+        }
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return sections[0]
+        } else if section == 1 {
+            return sections[1]
+        } else if section == 2 {
+            return sections[2]
+        } else {
+            return nil
+        }
+    }
+    
+}
--- Cookid/Cookid/ViewController/MyExpenseViewController.swift
+++ Cookid/Cookid/ViewController/MyExpenseViewController.swift
@@ -7,26 +7,126 @@
 
 import UIKit
 import FSCalendar
+import SnapKit
+import Then
 
 class MyExpenseViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print("드디어 된다.")
+    var dummyData = DummyData.shared
 
-        // Do any additional setup after loading the view.
+    var dineOutMeals : [Meal] = DummyData.shared.myMeals.filter{$0.mealType == .dineOut}
+    var dineInMeals : [Meal] = DummyData.shared.myMeals.filter{$0.mealType == .dineIn}
+    var shopping : [GroceryShopping] = DummyData.shared.myShoppings
+
+    var sections = ["집밥", "외식", "마트털이"]
+
+    
+    var mealDates : [Date?] = []
+    var shoppingDates : [Date?] = []
+    
+    var selectedDineInMeals : [Meal?] = []
+    var selectedDineOutMeals : [Meal?] = []
+    var selectedShopping : [GroceryShopping?] = []
+    
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter
+    }()
+    
+    let monthlyExpenseLabel = UILabel().then{
+        $0.text = "\(DummyData.shared.singleUser.priceGoal)" + "원"
+        $0.textColor = .black
+        $0.font = .boldSystemFont(ofSize: 40)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    let averageDailyExpenseLabel = UILabel().then{
+        $0.text = "하루평균 지출 금액은 5000원입니다."
+        $0.textColor = .darkGray
+        $0.font = .systemFont(ofSize: 15)
     }
-    */
-
+    
+    lazy var calendar = FSCalendar().then{
+        $0.delegate = self
+        $0.dataSource = self
+        $0.backgroundColor = .white
+        $0.locale = Locale(identifier: "ko_KR")
+        $0.appearance.headerDateFormat = "yyyy년 MM월"
+        $0.select(Date())
+        $0.scope = .month
+        $0.allowsMultipleSelection = false
+        $0.appearance.headerMinimumDissolvedAlpha = 0.0
+        $0.appearance.titleTodayColor = .black
+    }
+    
+    lazy var tableView = UITableView().then{
+        $0.delegate = self
+        $0.dataSource = self
+        
+        $0.rowHeight = UITableView.automaticDimension
+        $0.estimatedRowHeight = UITableView.automaticDimension
+        
+        $0.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.identifier)
+        
+        $0.panGestureRecognizer.require(toFail: self.scopeGesture)
+    }
+    
+     lazy var scopeGesture: UIPanGestureRecognizer = {
+        [unowned self] in
+        let panGesture = UIPanGestureRecognizer(target: self.calendar, action: #selector(self.calendar.handleScopeGesture(_:)))
+        panGesture.delegate = self
+        panGesture.minimumNumberOfTouches = 1
+        panGesture.maximumNumberOfTouches = 2
+        return panGesture
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpConstraint()
+    }
+    
+    deinit {
+        print("\(#function)")
+    }
+    
+    
+    //MARK: - constraints Setup
+    func setUpConstraint() {
+        self.view.addSubview(monthlyExpenseLabel)
+        self.view.addSubview(averageDailyExpenseLabel)
+        self.view.addSubview(calendar)
+        self.view.addSubview(tableView)
+                
+        //Gesture 추가
+        self.view.addGestureRecognizer(self.scopeGesture)
+        self.tableView.panGestureRecognizer.require(toFail: self.scopeGesture)
+        
+        
+        //monthlyExpenseLabel constraint
+        self.monthlyExpenseLabel.snp.makeConstraints{
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(10)
+            $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).offset(20)
+        }
+        
+        //averageDailyExpenseLabel constraint
+        self.averageDailyExpenseLabel.snp.makeConstraints{
+            $0.top.equalTo(monthlyExpenseLabel.snp.bottom)
+            $0.leading.trailing.equalTo(monthlyExpenseLabel)
+        }
+        
+        //calnedar constraint
+        self.calendar.snp.makeConstraints{
+            $0.top.equalTo(averageDailyExpenseLabel.snp.bottom).offset(16)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(UIDevice.current.model.hasPrefix("iPad") ? 400 : 300)
+        }
+        
+        //tableview constraint
+        self.tableView.snp.makeConstraints{
+            $0.top.equalTo(calendar.snp.bottom)
+            $0.leading.trailing.equalTo(calendar)
+            $0.bottom.equalToSuperview()
+        }
+    }
 }
