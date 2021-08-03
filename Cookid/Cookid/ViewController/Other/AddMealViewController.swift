8e430ae0a346d661d3e244dd8276514dce611f1c
Developer-Paul-t <phs880623@gmail.com>
2021-08-03 13:37:26 +0900

    피커뷰 수정

--- Cookid/Cookid/ViewController/Other/AddMealViewController.swift
+++ Cookid/Cookid/ViewController/Other/AddMealViewController.swift
@@ -10,6 +10,7 @@ import RxSwift
 import RxCocoa
 import NSObject_Rx
 
+
 class AddMealViewController: UIViewController, ViewModelBindable, StoryboardBased {
     
     var meal: Meal?
@@ -17,27 +18,48 @@ class AddMealViewController: UIViewController, ViewModelBindable, StoryboardBase
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var dimmingButton: UIButton!
-    
-    
     @IBOutlet weak var addPhotoButton: UIButton!
     @IBOutlet weak var addPictureButton: UIButton!
     @IBOutlet weak var isDineInSwitch: UISwitch!
     @IBOutlet weak var completionButton: UIButton!
     @IBOutlet weak var priceStackView: UIStackView!
     @IBOutlet weak var announceLabel: UILabel!
-    
-    
-    // test
     @IBOutlet weak var dateTF: UITextField!
     @IBOutlet weak var mealtimeTF: UITextField!
     @IBOutlet weak var mealnameTF: UITextField!
     @IBOutlet weak var mealpriceTF: UITextField!
+  
+    // pickers
+    lazy var datePicker: UIDatePicker = {
+        let dp = UIDatePicker(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 200))
+        dp.addTarget(self, action: #selector(self.dateChanged), for: .allEvents)
+        dp.locale = Locale(identifier: "ko-KR")
+        dp.datePickerMode = .date
+        dp.maximumDate = Date()
+        dp.backgroundColor = .white
+        if #available(iOS 13.4, *) {
+            dp.preferredDatePickerStyle = .wheels
+        } else {
+            // Fallback on earlier versions
+        }
+        return dp
+    }()
     
-    // reactive
+    lazy var mealTimePicker: UIPickerView = {
+        let picker = UIPickerView(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 200))
+        picker.backgroundColor = .white
+        picker.subviews.first?.subviews.last?.backgroundColor = #colorLiteral(red: 1, green: 0.870105389, blue: 0.5648625297, alpha: 0.5029385385)
+        return picker
+    }()
+    
+    // properties
+    
+    var viewModel: AddMealViewModel!
     var selectedPhoto: Bool = false
     var selectedPictrue: Bool = false
     
-    var viewModel: AddMealViewModel!
+    
+    // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,17 +67,23 @@ class AddMealViewController: UIViewController, ViewModelBindable, StoryboardBase
         initialSetting()
     }
     
+    
     // MARK: - UI
+    
     private func configureUI() {
         scrollView.layer.cornerRadius = 8
         contentView.layer.cornerRadius = 8
         addPhotoButton.layer.cornerRadius = 8
         addPictureButton.layer.cornerRadius = 8
+        dateTF.inputView = datePicker
+        dateTF.inputView?.backgroundColor = .white
+        mealtimeTF.inputView = mealTimePicker
+        settingPickerInTextField(dateTF)
+        settingPickerInTextField(mealtimeTF)
     }
     
     private func initialSetting() {
         guard let meal = meal else { return }
-        
         dateTF.text = convertDateToString(format: "MM월 dd일", date: meal.date)
         mealtimeTF.text = meal.mealTime.rawValue
         mealnameTF.text = meal.name
@@ -63,7 +91,56 @@ class AddMealViewController: UIViewController, ViewModelBindable, StoryboardBase
         isDineInSwitch.isOn = mealTypeToBool(meal.mealType)
     }
     
+    private func settingPickerInTextField(_ textfield: UITextField) {
+        
+        let buttonImage = UIImage(systemName: "checkmark.circle.fill")
+        
+        let doneButton = UIButton()
+        doneButton.setImage(buttonImage, for: .normal)
+        doneButton.addTarget(self, action: #selector(pickerDone), for: .touchUpInside)
+        doneButton.contentVerticalAlignment = .fill
+        doneButton.contentHorizontalAlignment = .fill
+        doneButton.imageEdgeInsets = UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
+        doneButton.tintColor = .systemGreen
+        
+        let doneView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 45))
+        doneView.backgroundColor = .white
+        doneView.roundCorners(corners: [.topLeft, .topRight], radius: 20)
+        
+        doneView.addSubview(doneButton)
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
+        doneButton.topAnchor.constraint(equalTo: doneView.topAnchor, constant: 11).isActive = true
+        doneButton.rightAnchor.constraint(equalTo: doneView.rightAnchor, constant: -11).isActive = true
+        doneButton.widthAnchor.constraint(equalToConstant: 36).isActive = true
+        doneButton.heightAnchor.constraint(equalToConstant: 36).isActive = true
+
+        textfield.inputAccessoryView = doneView
+        
+    }
+    
+    // MARK: - Action
+    
+    @objc func pickerDone() {
+        dateTF.resignFirstResponder()
+        mealtimeTF.resignFirstResponder()
+    }
+    
+    @objc func dateChanged() {
+        dateTF.text = "\(datePicker.date)"
+    }
+    
+    // MARK: - Binding
+    
     func bindViewModel() {
+        
+        Observable.of(MealTime.allCases.map { $0.rawValue })
+            .bind(to: mealTimePicker.rx.itemTitles) { [unowned self] row, element in
+                self.mealtimeTF.text = element
+                return element
+            }
+            .disposed(by: rx.disposeBag)
+            
+        
         
         dimmingButton.rx.tap
             .subscribe(onNext: {
