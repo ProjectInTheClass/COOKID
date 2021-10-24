029e526de8fe33c9d2b8bf1b34f3a313f1954e95
Developer-Paul-t <phs880623@gmail.com>
2021-10-24 20:45:56 +0900

    expandable tableView 제작 완료 답글보기버튼 구현 및 애니메이션 수정

--- Cookid/Cookid/ViewController/Post/CommentViewController.swift
+++ Cookid/Cookid/ViewController/Post/CommentViewController.swift
@@ -15,12 +15,14 @@ import Then
 import RxKeyboard
 
 class CommentViewController: UIViewController, View {
-    static let commentCell = "commentCell"
+    static let subCommentCell = "subCommentCell"
+    static let parentCommentCell = "parentCommentCell"
     
     private let commentInputTextFieldView = CommentInputView(frame: .zero)
     
-    private let tableView = UITableView(frame: .zero, style: .grouped).then {
-        $0.register(CommentTableViewCell.self, forCellReuseIdentifier: CommentViewController.commentCell)
+    private let tableView = UITableView(frame: .zero, style: .plain).then {
+        $0.register(CommentTableViewCell.self, forCellReuseIdentifier: subCommentCell)
+        $0.register(ParentCommentTableViewCell.self, forCellReuseIdentifier: parentCommentCell)
         $0.separatorStyle = .none
         $0.allowsSelection = false
         $0.backgroundColor = .systemBackground
@@ -32,10 +34,10 @@ class CommentViewController: UIViewController, View {
 
     var disposeBag = DisposeBag()
     
-    var commentHeaderViews = [CommentHeaderView]() {
+    var commentSections = [CommentSection]() {
         didSet {
             self.tableView.reloadData()
-            commentHeaderViews.isEmpty ? activityIndicator.startAnimating() : activityIndicator.stopAnimating()
+            commentSections.isEmpty ? activityIndicator.startAnimating() : activityIndicator.stopAnimating()
         }
     }
 
@@ -48,6 +50,8 @@ class CommentViewController: UIViewController, View {
     private func configureUI() {
         navigationItem.title = "댓글 보기"
         view.backgroundColor = .systemBackground
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     private func makeConstraints() {
@@ -90,38 +94,66 @@ class CommentViewController: UIViewController, View {
         
         reactor.state.map { $0.commentSections }
         .observe(on: MainScheduler.instance)
-        .do(onNext: { [unowned self] section in
-            let headerViews = section
-                .map { $0.header }
-                .map { CommentCellReactor(post: reactor.post, comment: $0, commentService: reactor.commentService, userService: reactor.userService) }
-                .map { reactor -> CommentHeaderView in
-                    let commentHeaderView = CommentHeaderView(frame: .zero)
-                    commentHeaderView.reactor = reactor
-                    return commentHeaderView
-                }
-            self.commentHeaderViews = headerViews
+        .withUnretained(self)
+        .bind(onNext: { owner, sections in
+            owner.commentSections = sections
         })
-        .bind(to: tableView.rx.items(dataSource: reactor.dataSource))
         .disposed(by: disposeBag)
         
-        tableView.rx.setDelegate(self)
-            .disposed(by: disposeBag)
     }
 
 }
 
-extension CommentViewController: UITableViewDelegate {
+extension CommentViewController: UITableViewDataSource, UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return commentHeaderViews[section]
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return commentSections.count
     }
     
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return UITableView.automaticDimension
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let commentSection = commentSections[section]
+        if commentSection.isOpened {
+            // 0번째는 section의 헤더, 나머지는 items이기 때문에 items 숫자 + 헤더 때문에 + 1
+            return commentSection.items.count + 1
+        } else {
+            // 헤더만
+            return 1
+        }
     }
     
-    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let reactor = reactor else { return UITableViewCell() }
+        if indexPath.row == 0 {
+            
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CommentViewController.parentCommentCell, for: indexPath) as? ParentCommentTableViewCell else { return UITableViewCell() }
+            let headerComment = commentSections[indexPath.section].header
+            cell.reactor = CommentCellReactor(post: reactor.post, comment: headerComment, commentService: reactor.commentService, userService: reactor.userService)
+            
+            if commentSections[indexPath.section].items.isEmpty {
+                cell.detailSubCommentButton.isEnabled = false
+            } else {
+                cell.detailSubCommentButton.isEnabled = true
+            }
+            
+            cell.detailSubCommentButton.rx.tap
+                .withUnretained(self)
+                .bind(onNext: { owner, _ in
+                    UIView.setAnimationsEnabled(false)
+                    owner.commentSections[indexPath.section].isOpened = !owner.commentSections[indexPath.section].isOpened
+                    tableView.reloadSections([indexPath.section], with: .fade)
+                    UIView.setAnimationsEnabled(true)
+                })
+                .disposed(by: cell.disposeBag)
+            
+            return cell
+            
+        } else {
+            // indexPath.row에 -1을 해야 out of range가 안뜬다. 헤더 때문에 row는 +1을 했지만, item는 실제로 그만큼의 데이터가 없다.
+            let subComment = commentSections[indexPath.section].items[indexPath.row - 1]
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CommentViewController.subCommentCell, for: indexPath) as? CommentTableViewCell else { return UITableViewCell() }
+            cell.reactor = CommentCellReactor(post: reactor.post, comment: subComment, commentService: reactor.commentService, userService: reactor.userService)
+            return cell
+        }
     }
     
     // 테이블뷰의 스타일이 그룹드 일 때, 섹션 사이의 갭이 생긴다. 그걸 없애주는 코드
