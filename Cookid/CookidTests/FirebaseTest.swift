98b4beb73c6ab5a56e537bd376bec487626f7827
Developer-Paul-t <phs880623@gmail.com>
2021-11-10 22:45:00 +0900

    firebase unit test start

--- Cookid/CookidTests/FirebaseTest.swift
+++ Cookid/CookidTests/FirebaseTest.swift
@@ -1,13 +1,13 @@
 //
-//  RepoTests.swift
+//  FirebaseTest.swift
 //  CookidTests
 //
-//  Created by 박형석 on 2021/10/06.
+//  Created by 박형석 on 2021/11/10.
 //
 
 import XCTest
 
-class RepoTests: XCTestCase {
+class FirebaseTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
